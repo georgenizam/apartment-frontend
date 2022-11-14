36c2e058f4372414903f056fe9692ce9a53d9152
georgenizam <qwqw37137@gmail.com>
2022-11-14 22:06:42 +0300

    init

---
+++ nuxt.config.js
@@ -0,0 +1,31 @@
+export default {
+  head: {
+    title: 'apartment',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+  router: {
+    prefetchLinks: false
+  },
+  css: [],
+  plugins: [],
+  components: true,
+  buildModules: [
+    '@nuxt/typescript-build',
+  ],
+  modules: [
+    '@nuxtjs/axios',
+  ],
+  axios: {
+    baseURL: '/',
+  },
+  build: {},
+}
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias architecto, at blanditiis
+    consectetur debitis deserunt doloribus, earum eos eveniet id mollitia nesciunt nisi officia
+    quibusdam quod totam velit, voluptates!
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'IndexPage',
+});
+</script>
